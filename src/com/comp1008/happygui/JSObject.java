0c64db29c8037d49e40d1a35fda1caf75343a297
insperatum@gmail.com <insperatum@gmail.com>
2013-04-23 18:45:23 +0100

    Commented, and added removeImage method

--- src/com/comp1008/happygui/JSObject.java
+++ src/com/comp1008/happygui/JSObject.java
@@ -1,17 +1,12 @@
 package com.comp1008.happygui;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-
-import org.json.JSONException;
 
 import android.app.Activity;
 import android.content.Context;
@@ -23,24 +18,217 @@ import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
+/* This class is used to handle communication between javascript and java.
+   Methods annotated with @JavascriptInterface are callable from javascript within the WebView */
+
 public class JSObject {
 	public final int CAMERA_PICTURE_INTENT = 1;
 	public final int GALLERY_IMAGE_INTENT = 2;
 	
-	public File cameraFile;
+	public File cameraFile; // File where camera intent saves a photograph
 	
-	private String callback;
+	private String callback = ""; // Javascript function to call with any returned data. Emptry string means no callback.
 	private WebView webView;
-	private Activity activity;
 	
-	public JSObject(Activity activity, WebView webView) {
-		this.activity = activity;
+	public JSObject(WebView webView) {
 		this.webView = webView;
 	}
+	
+	
+	
+	
 
+	// ----------- FUNCTIONS CALLABLE FROM JAVASCRIPT -----------------
+	
+	@JavascriptInterface
+	public void log(String msg) {
+		Log.d("JSObject", msg);
+	}	
+	
+	
+	// Save a JSON-formatted string to file "/pages/<name>"
+	@JavascriptInterface
+	public void setObject(String name, String json, String callback) {
+		String contents = json;
+		/*try {
+			contents = Translator.translate(json, Translator.FORMAT_JSON, Translator.FORMAT_XML);
+		} catch (Exception e) {
+			log("Could not translate JSON");
+			callbackError();
+			return;
+		}*/
+		
+		File file = new File(webView.getContext().getDir("pages", Context.MODE_PRIVATE), name);
+		
+		try {
+			FileWriter writer = new FileWriter(file);				
+			writer.write(contents);
+			writer.close();
+			callJavascriptFunction(callback, true);
+		} catch (IOException e) {
+			log("Could not save file");
+			callbackError();
+		}
+	}
+
+
+	@JavascriptInterface
+	public void setObject(String name, String json) {
+		// no callback
+		setObject(name, json, "");
+	}
+	
+	
+	
+	// Call callback with a JSON-formatted directory listing of /pages 
+	@JavascriptInterface
+	public void getObjects(String callback) {
+		File folder = webView.getContext().getDir("pages", Context.MODE_PRIVATE);
+		File[] list = folder.listFiles();
+		String[] names = new String[list.length];
+		for (int i = 0; i < list.length; i++) {
+			names[i] = list[i].getName();
+		}
+		callJavascriptFunction(callback, new Object[]{names});
+	}
+	
+	
+	// Call callback with the contents of file "/pages/<name>"
+	@JavascriptInterface
+	public void getObject(String name, String callback) {
+		File file = new File(webView.getContext().getDir("pages", Context.MODE_PRIVATE), name);
+		String text;
+		try {
+			//Read the file as a string
+			text = new Scanner(file).useDelimiter("\\A").next();
+		} catch (FileNotFoundException e) {
+			log("File not found!");
+			callbackError();
+			return;
+		}
+	
+		
+		callJavascriptFunction(callback, text);
+		/*
+		try {
+			String json = Translator.translate(text, Translator.FORMAT_XML, Translator.FORMAT_JSON);
+			System.out.println(json);
+			callJavascriptFunction(callback, json);
+		} catch (TranslationException e) {
+			log("Could not translate");
+			callbackError();
+		}*/
+	}
+
+	
+	
+	// Once an image has been removed from the editor, delete the local file.
+	@JavascriptInterface
+	public void removeImage(String src, String callback) {
+		this.callback = callback;
+		File file = new File(src);
+		if(file.delete()) {
+			callJavascriptFunction(callback, true);
+		} else {
+			log("Failed to delete image file: " + src);
+			callJavascriptFunction(callback, false);
+		}
+	}
+	
+	@JavascriptInterface
+	public void removeImage(String src) {
+		//no callback
+		removeImage(src, "");
+	}
+		
+	
+	
+	
+	// Translate a JSON formatted string representing the page to TouchDevelop code, and send to callback. 
+	@JavascriptInterface
+	public void translateToTouchDevelop(String json, String callback) {
+		try {
+			String output = Translator.translate(json, Translator.FORMAT_JSON, Translator.FORMAT_TOUCHDEVELOP);
+			callJavascriptFunction(callback, output);
+		} catch (TranslationException e) {
+			log(e.getMessage());
+			callbackError();
+		}
+	}
+
+	
+	/*@JavascriptInterface
+	public void translateToHTML(String json, String callback) {
+		try {
+			String output = Translator.translate(json, Translator.FORMAT_JSON, Translator.FORMAT_HTML);
+			callJavascriptFunction(callback, output);
+		} catch (TranslationException e) {
+			log(e.getMessage());
+			callbackError();
+		}
+	}*/
+	
+	
+	
+	// Capture a photo to import into the editor
+	@JavascriptInterface
+	public void getPhoto(String callback) {
+		this.callback = callback;
+		
+		String timeStamp = SimpleDateFormat.getDateTimeInstance().format(new Date());
+		cameraFile = new File(Environment.getExternalStorageDirectory(), "HappyGUI_capture_" + timeStamp + ".png");
+		// Initially saves the photo in external storage, as the user might want to keep it.
+		
+		Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(cameraFile));
+		((Activity) webView.getContext()).startActivityForResult(cameraIntent, CAMERA_PICTURE_INTENT);
+		// Start the image capture intent.
+	}
+	
+	
+
+	// Select a gallery image to import into the editor
+	@JavascriptInterface
+	public void getGalleryImage(String callback) {
+		this.callback = callback;
+
+		Intent gallIntent = new Intent(Intent.ACTION_GET_CONTENT);
+		gallIntent.setType("image/*"); // only display image files
+		
+		((Activity)webView.getContext()).startActivityForResult(gallIntent, GALLERY_IMAGE_INTENT);
+		// Start the gallery selection intent
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// ----------- FUNCTIONS FOR SENDING DATA BACK TO JAVASCRIPT -----------------
+	
+	
+	// Image/Camera request has succeeded. Send the location of the image back to javascript
+	public void gotImage(String src) {
+		callJavascriptFunction(callback, src);
+	}
+	
+		
+	// The request from javascript has failed. Run javascript:<callback>(false) 
+	public void callbackError() {
+		callJavascriptFunction(callback, false);
+	}
+	
+	
+	// Call a javascript function in the webview from java
 	public void callJavascriptFunction(String function, Object... args) {
 		log("Calling Javascript Function: " + function);
-		if(function != "") {
+	
+		if(! function.equalsIgnoreCase("")) {
 			String url = "javascript:" + function + "(";
 			for (int i = 0; i < args.length; i++) {
 				if (i > 0)
@@ -48,12 +236,14 @@ public class JSObject {
 				url += toJavascriptString(args[i]); 
 			}
 			url += ");";
-			log("Calling Javascript Function: " + url);
+			log("URL: " + url);
 			webView.loadUrl(url);
 		}
 	}
 
-	public String toJavascriptString(Object o) {
+	
+	// Convert an Object to a string so that it can be sent to javascript 
+	private String toJavascriptString(Object o) {
 		String out;
 		if(o instanceof String) {
 			out = "'" + ((String)o).replaceAll("\"", "\\\"") + "'"; 
@@ -71,146 +261,6 @@ public class JSObject {
 		return out;
 	}
 	
-	@JavascriptInterface
-	public void log(String msg) {
-		Log.d("Javascript", msg);
-	}
-
-	@JavascriptInterface
-	public void setObject(String name, String json) {
-		setObject(name, json, "");
-	}
 	
-	@JavascriptInterface
-	public void setObject(String name, String json, String callback) {
-		String contents;
-		try {
-			contents = json; 
-			System.out.println(contents);
-		} catch (Exception e) {
-			log("Could not translate JSON");
-			callJavascriptFunction(callback, false);
-			return;
-		}
-		
-		
-		File file = new File(activity.getDir("pages", Context.MODE_PRIVATE), name);
 
-		try {
-			FileWriter writer = new FileWriter(file);				
-			writer.write(contents);
-			writer.close();
-			callJavascriptFunction(callback, true);
-		} catch (IOException e) {
-			log("Could not save file");
-			callJavascriptFunction(callback, false);
-		}
-	}
-
-	@JavascriptInterface
-	public void getObjects(String callback) {
-		File folder = activity.getDir("pages", Context.MODE_PRIVATE);
-		File[] list = folder.listFiles();
-		String[] names = new String[list.length];
-		for (int i = 0; i < list.length; i++) {
-			names[i] = list[i].getName();
-		}
-		callJavascriptFunction(callback, new Object[]{names});
-	}
-	
-	@JavascriptInterface
-	public void getObject(String name, String callback) {
-		File file = new File(activity.getDir("pages", Context.MODE_PRIVATE), name);
-		String text;
-		try {
-			text = new Scanner(file).useDelimiter("\\A").next();
-			System.out.println(text);
-		} catch (FileNotFoundException e) {
-			log("File not found!");
-			callJavascriptFunction(callback, false);
-			return;
-		}
-	
-		try {
-			String json = text;
-			System.out.println(json);
-			callJavascriptFunction(callback, json);
-		} catch (Exception e) {
-			log("Could not translate");
-			callJavascriptFunction(callback, false);
-		}
-	}
-	
-	@JavascriptInterface
-	public void translateToHTML(String json, String callback) {
-		try {
-			String output = Translator.translate(json, Translator.FORMAT_JSON, Translator.FORMAT_HTML);
-			callJavascriptFunction(callback, output);
-		} catch (Exception e) {
-			log("Could not translate");
-			callJavascriptFunction(callback, false);
-		}
-	}
-	
-	@JavascriptInterface
-	public void translateToTouchDevelop(String json, String callback) {
-		try {
-			String output = Translator.translate(json, Translator.FORMAT_JSON, Translator.FORMAT_TOUCHDEVELOP);
-			callJavascriptFunction(callback, output);
-		} catch (Exception e) {
-			log("Could not translate");
-			callJavascriptFunction(callback, false);
-		}
-	}
-
-	@JavascriptInterface
-	public void getPhoto(String callback) { // Take a photo and send the
-											// image back to javascript
-		this.callback = callback;
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
-				.format(new Date());
-		cameraFile = new File(Environment.getExternalStorageDirectory(),
-				"HappyGUI_capture_" + timeStamp + ".png");
-
-		Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-				Uri.fromFile(cameraFile));
-		activity.startActivityForResult(cameraIntent, CAMERA_PICTURE_INTENT);
-	}
-	
-	@JavascriptInterface
-	public void getGalleryImage(String callback) {
-		this.callback = callback;
-		log("getGalleryImage");
-		Intent gallIntent=new Intent(Intent.ACTION_GET_CONTENT);
-		gallIntent.setType("image/*"); 
-		activity.startActivityForResult(gallIntent, GALLERY_IMAGE_INTENT);
-	}
-
-	public void addImage(File imageFile) { // Copies imageFile to local
-											// directory, then passes the
-											// new file URI to javascript
-											// addImage
-		Log.d("JSObject", "Adding image from:" + imageFile.toString());
-
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
-				.format(new Date());
-		File newFile = new File(activity.getDir("images", Context.MODE_PRIVATE), "image_"
-				+ timeStamp + ".png"); // replace filename with something
-										// more descriptive later.
-		FileChannel src;
-		FileChannel dst;
-		try {
-			src = new FileInputStream(imageFile).getChannel();
-			dst = new FileOutputStream(newFile).getChannel();
-			dst.transferFrom(src, 0, src.size());
-			src.close();
-			dst.close();
-			
-			Log.d("JSObject", "Image copied to:" + newFile.toString());
-			callJavascriptFunction(callback, "file://" + newFile.toString());
-		} catch (Exception e) {
-			Log.d("addPicture", "Error copying picture.");
-		}
-	}
 }
