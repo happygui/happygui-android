da27b5b477b174536292c1ab7b94f7d02e4770b8
insperatum@gmail.com <insperatum@gmail.com>
2013-04-26 20:01:26 +0100

    highlighting on code view

--- src/com/comp1008/happygui/Translator.java
+++ src/com/comp1008/happygui/Translator.java
@@ -15,16 +15,26 @@ public class Translator {
 	public static final int FORMAT_HTML = 3;
 	public static final int FORMAT_TOUCHDEVELOP = 4;
 
+	
+	private static final String STYLE_KEYWORD = "pink_txt";
+	private static final String STYLE_STRING = "blue_txt";
+	private static final String STYLE_NUMBER = "dgray_txt";
+	private static final String STYLE_COMMENT = "green_txt";
+	
+	private static final String START_LINE = "<span class=\"code_line\">";
+	private static final String END_LINE = "</span><br/>";
+	
+	
 
 	public static String translate(String input, int inputFormat, int outputFormat) throws TranslationException {
-
+		
 		if(inputFormat == FORMAT_JSON && outputFormat == FORMAT_TOUCHDEVELOP) {
 			return jsonToTouchDevelop(input);
 		}
 		
 		/*
 		if(inputFormat == FORMAT_JSON && outputFormat == FORMAT_TOUCHDEVELOP) {
-			String xml = translate(input, FORMAT_JSON, FORMAT_XML); // translate first JSON -> XML, then XML -> TouchDevelop
+			String xml = translate(input, FORMAT_JSON, FORMAT_XML); // translate first JSON &rarr; XML, then XML &rarr; TouchDevelop
 			Log.d("Translator", xml);
 			try {
 				return xmlToTouchDevelop(xml);
@@ -81,11 +91,16 @@ public class Translator {
 			data = new JSONObject(input);
 			int imageNum = 0; // the number of images on the page (for variable naming)
 			
-			String output = "action main()<br/>";
+			String output = START_LINE + stylise(STYLE_COMMENT, "// " + data.get("name").toString()) + END_LINE; // Include the name of the page as a comment
+			
+			output += START_LINE + stylise(STYLE_KEYWORD, "action") + " main()" + END_LINE;
+			
 			if(data.has("backgroundColor")) {
-				output += "wall -> set background(" + colorToTouchDevelop(data.get("backgroundColor").toString(), 1) + ")<br/>";
+				output += START_LINE + "wall &rarr; set background(" + colorToTouchDevelop(data.get("backgroundColor").toString(), 1) + ")" + END_LINE;
 			}
-			output += "var page := media -> create picture(480, 600)<br/>";
+			output += START_LINE + stylise(STYLE_KEYWORD, "var")
+					+ " page := media &rarr; create picture(" + stylise(STYLE_NUMBER, 480) + ", " + stylise(STYLE_NUMBER, 600)
+					+ ")" + END_LINE;
 			
 			JSONArray elements = data.getJSONArray("elements");
 			// Loop through list of elements in the page
@@ -103,81 +118,92 @@ public class Translator {
 						int r = Integer.parseInt(element.get("r").toString()); //circle radius
 						
 						// first draw fill
-						output += "page -> fill ellipse (" + (x - r)	// left
-								+ ", " + (y - r)						// top
-								+ ", " + (2 * r)						// width
-								+ ", " + (2 * r)						// height
-								+ ", 0"									// angle
+						output += START_LINE + "page &rarr; fill ellipse ("
+								+ stylise(STYLE_NUMBER, (x - r))		// left
+								+ ", " + stylise(STYLE_NUMBER, (y - r)) // top
+								+ ", " + stylise(STYLE_NUMBER, (2 * r))	// width
+								+ ", " + stylise(STYLE_NUMBER, (2 * r))	// height
+								+ ", " + stylise(STYLE_NUMBER, 0)		// angle
 								+ ", " + colorToTouchDevelop(element.get("backgroundColor").toString(), 0.8) // fill colour
-								+ ")<br/>";
+								+ ")" + END_LINE;
 						
 						// ...then draw border
-						output += "page -> draw ellipse (" + (x - r)	// left
-								+ ", " + (y - r)						// top
-								+ ", " + (2 * r)						// width
-								+ ", " + (2 * r)						// height
-								+ ", 0"									// angle
+						output += START_LINE + "page &rarr; draw ellipse ("
+								+ stylise(STYLE_NUMBER, (x - r))		// left
+								+ ", " + stylise(STYLE_NUMBER, (y - r))	// top
+								+ ", " + stylise(STYLE_NUMBER, (2 * r))	// width
+								+ ", " + stylise(STYLE_NUMBER, (2 * r))	// height
+								+ ", " + stylise(STYLE_NUMBER, 0)		// angle
 								+ ", " + colorToTouchDevelop(element.get("borderColor").toString(), 0.8)	// border colour
-								+ ", " + element.get("borderThickness").toString()							// border thickness
-								+ ")<br/>";
+								+ ", " + stylise(STYLE_NUMBER, element.get("borderThickness").toString())	// border thickness
+								+ ")" + END_LINE;
 
 					} else if(type.equals("rect")) {
 						// Generate TouchDevelop code for rectangle
 						
 						// first draw fill
-						output += "page -> fill rect (" + element.get("x").toString()	// left
-								+ ", " + element.get("y").toString()					// top
-								+ ", " + element.get("width").toString()				// width
-								+ ", " + element.get("height").toString()				// height
-								+ ", 0"													// angle
+						output += START_LINE + "page &rarr; fill rect ("
+								+ stylise(STYLE_NUMBER, element.get("x").toString())							// left
+								+ ", " + stylise(STYLE_NUMBER, element.get("y").toString())						// top
+								+ ", " + stylise(STYLE_NUMBER, element.get("width").toString())					// width
+								+ ", " + stylise(STYLE_NUMBER, element.get("height").toString())				// height
+								+ ", " + stylise(STYLE_NUMBER, 0)												// angle
 								+ ", " + colorToTouchDevelop(element.get("backgroundColor").toString(), 0.8)	//fill colour
-								+ ")<br/>";
+								+ ")" + END_LINE;
 						
 						// ... then draw border
-						output += "page -> draw rect (" + element.get("x").toString()	// left
-								+ ", " + element.get("y").toString()					// top
-								+ ", " + element.get("width").toString()				// width
-								+ ", " + element.get("height").toString()				// height
-								+ ", 0"													// angle
-								+ ", " + colorToTouchDevelop(element.get("borderColor").toString(), 0.8)	// border colour
-								+ ", " + element.get("borderThickness").toString()							// border thickness
-								+ ")<br/>";
+						output += START_LINE + "page &rarr; draw rect ("
+								+ stylise(STYLE_NUMBER, element.get("x").toString())							// left
+								+ ", " + stylise(STYLE_NUMBER, element.get("y").toString())						// top
+								+ ", " + stylise(STYLE_NUMBER, element.get("width").toString())					// width
+								+ ", " + stylise(STYLE_NUMBER, element.get("height").toString())				// height
+								+ ", " + stylise(STYLE_NUMBER, 0)												// angle
+								+ ", " + colorToTouchDevelop(element.get("borderColor").toString(), 0.8)		// border colour
+								+ ", " + stylise(STYLE_NUMBER, element.get("borderThickness").toString())		// border thickness
+								+ ")" + END_LINE;
 
 					}  else if(type.equals("text")) {
 						// Generate TouchDevelop code for text
 						
-						output += "page -> draw text (" + element.get("x").toString()	// left
-								+ ", " + element.get("y").toString()					// top
-								+ ", \"" + element.get("text").toString() + "\""		// text
-								+ ", " + element.get("fontSize").toString()				// font size
-								+ ", 0"													// angle
-								+ ", " + colorToTouchDevelop(element.get("fontColor").toString(), 0.8)	// colour
-								+ "))<br/>";
+						output += START_LINE + "page &rarr; draw text ("
+								+ stylise(STYLE_NUMBER, element.get("x").toString())							// left
+								+ ", " + stylise(STYLE_NUMBER, element.get("y").toString())						// top
+								+ ", " + stylise(STYLE_STRING, "\"" + element.get("text").toString() + "\"")	// text
+								+ ", " + stylise(STYLE_NUMBER, element.get("fontSize").toString())				// font size
+								+ ", " + stylise(STYLE_NUMBER, 0)												// angle
+								+ ", " + colorToTouchDevelop(element.get("fontColor").toString(), 0.8)			// colour
+								+ ")" + END_LINE;
 						
 					} else if(type.equals("image")) {
 						// Generate TouchDevelop code for image
 						
 						imageNum ++;
-						output += "var image" + imageNum + " := web -> download picture (\"Image " + imageNum + " URL\")<br/>";
-						output += "image" + imageNum + " -> resize(" + element.get("width").toString()
-								+ ", " + element.get("height").toString()
-								+ ")<br/>";
+						output += START_LINE + stylise(STYLE_KEYWORD, "var") + " image" + imageNum
+								+ " := web &rarr; download picture ("
+								+ stylise(STYLE_STRING, "\"Image " + imageNum + " URL\"")
+								+ ")" + END_LINE;																// get picture
 						
-						output += "page -> blend (image" + imageNum
-								+ ", " + element.get("x").toString()
-								+ ", " + element.get("y").toString()
-								+ ", 0"	// angle
-								+ ", 1" // opacity
-								+ ")<br/>";
+						output += START_LINE + "image" + imageNum + " &rarr; resize("
+								+ stylise(STYLE_NUMBER, element.get("width").toString())
+								+ ", " + stylise(STYLE_NUMBER, element.get("height").toString())				//resize to correct proportions
+								+ ")" + END_LINE;
+						
+						output += START_LINE + "page &rarr; blend ("											// add image to 'page'
+								+ "image" + imageNum															
+								+ ", " + stylise(STYLE_NUMBER, element.get("x").toString())						// left
+								+ ", " + stylise(STYLE_NUMBER, element.get("y").toString())						// top
+								+ ", " + stylise(STYLE_NUMBER, 0)												// angle
+								+ ", " + stylise(STYLE_NUMBER, 1)												// opacity
+								+ ")" + END_LINE;
 						
 					} else {
-						output += "// " + type + " <br/>";	// element of unknown type
+						output += START_LINE + stylise(STYLE_COMMENT, "// " + type) + END_LINE;					// element of unknown type
 					}
 				}
 			}
 			
 			
-			output += "page -> post to wall";
+			output += START_LINE + "page &rarr; post to wall" + END_LINE;
 			return output;
 			
 		} catch (JSONException e) {
@@ -186,7 +212,7 @@ public class Translator {
 	}
 	
 	
-	// Translate a colour from format "rgb(255,255,255)" to "color -> from argb(alpha,1,1,1)"
+	// Translate a colour from format "rgb(255,255,255)" to "color &rarr; from argb(alpha,1,1,1)"
 	public static String colorToTouchDevelop(String col, double alpha) {
 		int rLeft = col.indexOf("(") + 1;
 		int gLeft = col.indexOf(",") + 1;
@@ -200,13 +226,21 @@ public class Translator {
 		DecimalFormat formatter = new DecimalFormat("#.##"); // Use up to 2 decimal places for each channel
 
 		if(alpha == 1) {
-			return "color -> from rgb (" + formatter.format(r) + ", " + formatter.format(g) + ", " + formatter.format(b) + ")";
+			return "color &rarr; from rgb (" + stylise(STYLE_NUMBER, formatter.format(r)) + ", " + stylise(STYLE_NUMBER, formatter.format(g)) + ", " + stylise(STYLE_NUMBER, formatter.format(b)) + ")";
 		} else {
-			return "color -> from argb (" + formatter.format(alpha) + ", " + formatter.format(r) + ", " + formatter.format(g) + ", " + formatter.format(b) + ")";
+			return "color &rarr; from argb (" + stylise(STYLE_NUMBER, formatter.format(alpha)) + ", " + stylise(STYLE_NUMBER, formatter.format(r)) + ", " + stylise(STYLE_NUMBER, formatter.format(g)) + ", " + stylise(STYLE_NUMBER, formatter.format(b)) + ")";
 		}
 	}
 	
 	
+	// Return the html for a particular string, coloured by the type it represents
+	public static String stylise(String style, String text) {
+		return "<span class=\"" + style + "\">" + text + "</span>";
+	}
+	
+	public static String stylise(String style, Integer num) {
+		return stylise(style, num.toString());
+	}
 	
 	
 	/*
@@ -216,7 +250,7 @@ public class Translator {
 		int imageNum = 0; // the number of images on the page (for variable naming)
 		
 		String output = "action main()";
-		output += "var page := media -> create picture(480, 600)<br/>";
+		output += "var page := media &rarr; create picture(480, 600)<br/>";
 
 		while(parser.getEventType() != XmlPullParser.END_DOCUMENT) {
 			switch(parser.getEventType()) {			
@@ -231,14 +265,14 @@ public class Translator {
 				}
 				if(type.equals("circle")) {
 					// Generate TouchDevelop code for circle
-					output += "page -> fill ellipse (" + parser.getAttributeValue(null, "x")
+					output += "page &rarr; fill ellipse (" + parser.getAttributeValue(null, "x")
 							+ ", " + parser.getAttributeValue(null, "y")
 							+ ", " + parser.getAttributeValue(null, "radius")
 							+ ", " + parser.getAttributeValue(null, "radius")
 							+ ", 0"
 							+ ", " + colorToTouchDevelop(parser.getAttributeValue(null, "backgroundColor"), 0.8)
 							+ ")<br/>";
-					output += "page -> draw ellipse (" + parser.getAttributeValue(null, "x")
+					output += "page &rarr; draw ellipse (" + parser.getAttributeValue(null, "x")
 							+ ", " + parser.getAttributeValue(null, "y")
 							+ ", " + parser.getAttributeValue(null, "radius")
 							+ ", " + parser.getAttributeValue(null, "radius")
@@ -249,14 +283,14 @@ public class Translator {
 
 				} else if(type.equals("rect")) {
 					// Generate TouchDevelop code for rectangle
-					output += "page -> fill rect (" + parser.getAttributeValue(null, "x")
+					output += "page &rarr; fill rect (" + parser.getAttributeValue(null, "x")
 							+ ", " + parser.getAttributeValue(null, "y")
 							+ ", " + parser.getAttributeValue(null, "width")
 							+ ", " + parser.getAttributeValue(null, "height")
 							+ ", 0"
 							+ ", " + colorToTouchDevelop(parser.getAttributeValue(null, "backgroundColor"), 0.8)
 							+ ")<br/>";
-					output += "page -> draw rect (" + parser.getAttributeValue(null, "x")
+					output += "page &rarr; draw rect (" + parser.getAttributeValue(null, "x")
 							+ ", " + parser.getAttributeValue(null, "y")
 							+ ", " + parser.getAttributeValue(null, "width")
 							+ ", " + parser.getAttributeValue(null, "height")
@@ -267,7 +301,7 @@ public class Translator {
 
 				}  else if(type.equals("text")) {
 					// Generate TouchDevelop code for text
-					output += "page -> draw text (" + parser.getAttributeValue(null, "x")
+					output += "page &rarr; draw text (" + parser.getAttributeValue(null, "x")
 							+ ", " + parser.getAttributeValue(null, "y")
 							+ ", \"< PUT TEXT HERE >\""
 							+ ", " + parser.getAttributeValue(null, "fontSize")
@@ -277,11 +311,11 @@ public class Translator {
 				} else if(type.equals("image")) {
 					// Generate TouchDevelop code for image
 					imageNum ++;
-					output += "var image" + imageNum + " := web -> download picture (\"<URL>\")<br/>";
-					output += "image" + imageNum + " -> resize(" + parser.getAttributeValue(null, "width")
+					output += "var image" + imageNum + " := web &rarr; download picture (\"<URL>\")<br/>";
+					output += "image" + imageNum + " &rarr; resize(" + parser.getAttributeValue(null, "width")
 							+ ", " + parser.getAttributeValue(null, "height")
 							+ ")<br/>";
-					output += "page -> blend (image" + imageNum
+					output += "page &rarr; blend (image" + imageNum
 							+ ", " + parser.getAttributeValue(null, "x")
 							+ ", " + parser.getAttributeValue(null, "y")
 							+ ", 0"
@@ -304,7 +338,7 @@ public class Translator {
 
 		}
 
-		output += "page -> post to wall";
+		output += "page &rarr; post to wall";
 		return output;
 	}
 */
@@ -352,7 +386,7 @@ public class Translator {
 							+ "</font>"
 							+ "</div>";	
 				} else {
-					output += "<!--" + parser.getName() + " tag --><br/>";
+					output += "<!--" + parser.getName() + " tag -&rarr;<br/>";
 				}
 				break;
 
